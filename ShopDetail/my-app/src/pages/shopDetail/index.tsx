acadd279f9dcf58e447757fd42edf4064433cc59
jingdd-boop <1736929925@qq.com>
2022-03-11 19:28:41 +0800

    feat: 初始化

---
+++ ShopDetail/my-app/src/pages/shopDetail/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function ShopDetail() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>hh</div>
+      </header>
+    </div>
+  );
+}
+
+export default ShopDetail;
